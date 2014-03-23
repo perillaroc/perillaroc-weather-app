b4206c5122904af3c6d8935b84ef2af62e83f812
perillaroc <perillaroc@gmail.com>
2014-03-23 22:03:23 +0800

    Add date and time

--- public/js/index.js
+++ public/js/index.js
@@ -19,10 +19,15 @@ $(document).ready(function(){
                 var weather_icon_url = 'http://openweathermap.org/img/w/'+weather.icon+'.png';
             var wind = data.wind;
 
+            var now = new Date();
+            var now_str = (now.getMonth()+1)+'月'+now.getDay()+'日 '+now.getHours()+':'+now.getMinutes();
+
             $('<div class="col-md-4"></div>')
-                .append('<div class="row"><div class="col-md-12">'+main.temp+"&deg;C"+'</div></div>')
-                .append('<div class="row"><div class="col-md-12"><img src='+weather_icon_url+'>'+weather.description+'</div></div>')
-                .append('<div class="row"><div class="col-md-12">'+wind.speed+'m/s</div></div>')
+                .append('<div class="row"><div class="col-md-12">'+now_str+'</div></div>')
+                .append('<div class="row"><div class="col-md-12">天气：<img src='+weather_icon_url+'>'+weather.description+'</div></div>')
+                .append('<div class="row"><div class="col-md-12">温度：'+main.temp+"&deg;C"+'</div></div>')
+                .append('<div class="row"><div class="col-md-12">风速：'+wind.speed+'m/s</div></div>')
+                .append('<div class="row"><div class="col-md-12">湿度：'+main.humidity+'%</div></div>')
                 .appendTo('<div class="row"></div>')
                 .appendTo('.weather');
         }
